d3606e180df7b68a28e6a3285328e7303a2a063d
Andrei Porumb <anporumb@microsoft.com>
2017-04-11 13:22:25 -0700

    have umock_c compilable under vs 2017

--- tests/umock_c_gen_func_decl_int/umock_c_gen_func_decl_int.c
+++ tests/umock_c_gen_func_decl_int/umock_c_gen_func_decl_int.c
@@ -2,8 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #ifdef __cplusplus
+#include <cstdlib>
 #include <cstdint>
 #else
+#include <stdlib.h>
 #include <stdint.h>
 #endif
 
